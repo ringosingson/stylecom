d60d47a809dfdb1802be1825c1f80cf099d8d821
Ringo Singson <ringosingson@live.com>
2019-04-23 02:50:47 +0530

    displaying the product in the UI from json

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+//variables
+
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+// cart
+let cart = [];
+
 // getting the products
 class Products {
   async getProducts() {
@@ -18,6 +32,37 @@ class Products {
     }
   }
 }
+// display products
+class UI {
+  displayProducts(products) {
+    let result = "";
+    products.forEach(product => {
+      result += `
+     <!-- single product -->
+        <article class="product">
+          <div class="img-container">
+            <img
+              src=${product.image}
+              alt="product"
+              class="product-img"
+            />
+            <button class="bag-btn" data-id=${product.id}>
+              <i class="fas fa-shopping-cart"></i>
+              add to bag
+            </button>
+          </div>
+          <h3>${product.title}</h3>
+          <h4>$${product.price}</h4>
+        </article>
+        <!--end of single product -->
+     `;
+    });
+    productsDOM.innerHTML = result;
+  }
+}
+//local storage
+class Storage {}
+
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
